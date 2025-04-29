694a1e1e923943d4f16cf54063985b75c6150a5e
Aldy0226 <putra.jaya.ft24@mail.umy.ac.id>
2025-04-29 15:20:01 +0700

    Membuat object untuk memunculkan hasil akhir

--- IntroduceOOP.cpp
+++ IntroduceOOP.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
 
-
-class Mahasiswa{
+class Mahasiswa
+{
     public:
         string nama;
         int nim;
         float nilai;
-         
+
+        void PrintData()
+        {
+            cout << "Nama : " << nama << endl;
+            cout << "NIM : " << nim << endl;
+            cout << "Nilai : " << nilai << endl;
+        }
+
 };
 
-int main(){
-    
+int main()
+{
+    Mahasiswa mhs1;
+    mhs1.nama = "Jhoneldy";
+    mhs1.nim = 1000;
+    mhs1.nilai = 90.5;
 }
